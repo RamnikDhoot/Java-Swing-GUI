816f2365ae2939b531f3d148f755dd0e39430a7c
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-11-08 16:36:36 +0000

    Created new class for frame and used main just to create an instance

--- Main.java
+++ Main.java
@@ -1,23 +1,10 @@
-import java.awt.Color;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame();
+        // JFrame frame = new JFrame();
 
-        frame.setTitle("Frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false); // Change size of window, not allowed
-        frame.setSize(420, 420);
-        frame.setVisible(true);
-
-        ImageIcon image = new ImageIcon("images/notepad-icon.jpg");
-        frame.setIconImage(image.getImage()); // changes icon of frame
-
-        frame.getContentPane().setBackground(Color.blue); // Changes colour of background
+        MyFrame myFrame = new MyFrame();
 
     }
 }
